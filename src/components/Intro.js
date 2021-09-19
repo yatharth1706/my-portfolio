5ce104a95f019c98420194d9e113f391a2f6da8c
yatharth <yatharthverma070@gmail.com>
2021-09-19 16:09:48 +0530

    Fix issue

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,13 +3,15 @@ import React from "react";
 function Intro() {
   return (
     <div className="w-full flex md:w-3/6 flex-col font-rubik pl-20 pr-20 md:pr-0 md:mt-20 mt-16 mb-10 mx-auto text-center md:text-left">
-      <span className = "text-xl mb-3">Hy! I Am</span>
-      <span style={{ fontWeight: "500" }} className = "text-4xl mb-3">Yatharth Verma</span>
+      <span className="text-xl mb-3">Hy! I Am</span>
+      <span style={{ fontWeight: "500" }} className="text-4xl mb-3">
+        Yatharth Verma
+      </span>
       <span style={{ fontSize: "18px", fontWeight: "200" }}>
         Full stack web developer and a learner. Believes in hard work
       </span>
       <button
-        className="text-white	bg-custom-light-blue p-2 rounded-3xl mt-6 w-60 mx-auto md:mx-0"
+        className="text-white	bg-custom-light-blue p-2 rounded-3xl mt-6 w-full sm:w-60 mx-auto md:mx-0"
         style={{ color: "white" }}
       >
         Download Resume
