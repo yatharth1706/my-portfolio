2fb121561bca27854fac320aba77123cc21a7125
yatharth <yatharthverma070@gmail.com>
2021-12-19 19:58:24 +0530

    fix issue

--- src/components/ProjectsConfig.js
+++ src/components/ProjectsConfig.js
@@ -2,9 +2,9 @@ import Emojigit from "../images/Emojigit.PNG";
 import HuluClone from "../images/HuluClone.PNG";
 import CPEditor from "../images/CPEditor.PNG";
 import DevBlogsPreview from "../images/DevBlogsPreview.PNG";
-import CloudSpacePreview from "../images/CloudspacePreview.PNG";
+import CloudSpacePreview from "../images/CloudspacePreview1.PNG";
 import PortfolioPreview from "../images/PortfolioPreview.PNG";
-import ChatSpacePreview from "../images/ChatSpacePreview1.png";
+import ChatSpacePreview from "../images/ChatSpacePreview.png";
 
 const projectsConfig = [
   {
@@ -14,25 +14,6 @@ const projectsConfig = [
       "Fast One to One messaging app built using NextJS, Styled Components, Firebase v8 (Firestore and Auth), and React Firebase hooks for realtime fetching of data from the database. ",
     liveDemoLink: "https://chat-space.vercel.app",
     githubLink: "https://github.com/yatharth1706/chatspace",
-  },
-  {
-    title: "Emoji Git CLI Tool",
-    previewImage: Emojigit,
-    description:
-      "In this project, I built Interactive CLI Tool for writing emojis in commit messages.It gives user some list of emojis in terminal before making commit. From that list user can select emojis according to the commit task he has done. After choosing that user can right commit message along with that emoji.",
-    liveDemoLink: "https://www.npmjs.com/package/emojigit",
-    githubLink: "https://github.com/yatharth1706/emojigit",
-    downloads:
-      "https://img.shields.io/npm/dt/emojigit.svg?style=flat-square)](https://www.npmjs.com/package/emojigit",
-  },
-
-  {
-    title: "DevBlogs",
-    previewImage: DevBlogsPreview,
-    description:
-      "This website is basically for sharing blogs with everyone. It gives us platform to write blogs in markdown language. Provides the preview also where we can see the final output of the blog post. It was kind of like a side project to refresh my frontend skills as well as backend skills. Tech stack using in this are React and Firebase",
-    liveDemoLink: "https://dev-blogs-blond.vercel.app/",
-    githubLink: "https://github.com/yatharth1706/DevBlogs",
   },
 
   {
@@ -45,12 +26,31 @@ const projectsConfig = [
   },
 
   {
+    title: "DevBlogs",
+    previewImage: DevBlogsPreview,
+    description:
+      "This website is basically for sharing blogs with everyone. It gives us platform to write blogs in markdown language. Provides the preview also where we can see the final output of the blog post. It was kind of like a side project to refresh my frontend skills as well as backend skills. Tech stack using in this are React and Firebase",
+    liveDemoLink: "https://dev-blogs-blond.vercel.app/",
+    githubLink: "https://github.com/yatharth1706/DevBlogs",
+  },
+
+  {
     title: "Portfolio",
     previewImage: PortfolioPreview,
     description:
       "Portfolio website built using ReactJS and TailwindCSS. Includes information about me and my skills along with side projects.",
     liveDemoLink: "https://my-portfolio-six-jet.vercel.app/",
     githubLink: "https://github.com/yatharth1706/my-portfolio",
+  },
+  {
+    title: "Emoji Git CLI Tool",
+    previewImage: Emojigit,
+    description:
+      "In this project, I built Interactive CLI Tool for writing emojis in commit messages.It gives user some list of emojis in terminal before making commit. From that list user can select emojis according to the commit task he has done. After choosing that user can right commit message along with that emoji.",
+    liveDemoLink: "https://www.npmjs.com/package/emojigit",
+    githubLink: "https://github.com/yatharth1706/emojigit",
+    downloads:
+      "https://img.shields.io/npm/dt/emojigit.svg?style=flat-square)](https://www.npmjs.com/package/emojigit",
   },
   {
     title: "Hulu Clone 2.0",
