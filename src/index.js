b19ba9713b56ac58f44c3918657e836470bba875
yatharth <yatharthverma070@gmail.com>
2023-04-13 21:53:24 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // eslint-disable-next-line import/no-unresolved
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/dist/react";
+// import { Analytics } from "@vercel/analytics/dist/react";
 
 ReactDOM.render(
   <React.StrictMode>
