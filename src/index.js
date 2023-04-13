b2a5901d04c69f9d8aac77fffe938891a489261a
yatharth <yatharthverma070@gmail.com>
2023-04-13 21:48:59 +0530

    add eslint disable line code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+// eslint-disable-next-line import/no-unresolved
 import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.render(
