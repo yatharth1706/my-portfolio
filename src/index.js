9ef376b1f5c9a816887a287162d01744ca057cb0
yatharth <yatharthverma070@gmail.com>
2023-04-16 18:52:58 +0530

    Add metadatas for link preview

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { inject } from "@vercel/analytics";
+import { Helmet } from "react-helmet";
 
 inject();
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <meta property="og:title" content="My Portfolio" />
+      <meta
+        property="og:description"
+        content="Checkout my portfolio. Built using ReactJS and Tailwind CSS"
+      />
+      <meta
+        property="og:image"
+        content="https://user-images.githubusercontent.com/32243289/232308492-450a80af-b9b8-4636-87d9-f1c4847ed721.jpg"
+      />
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
