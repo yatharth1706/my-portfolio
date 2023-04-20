a72907f159689644fd0268191ac7a18838bceaa1
yatharth <yatharthverma070@gmail.com>
2023-04-21 00:39:13 +0530

    moved meta tags to index.html

--- src/index.js
+++ src/index.js
@@ -4,31 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { inject } from "@vercel/analytics";
-import { Helmet } from "react-helmet";
 
 inject();
 ReactDOM.render(
   <React.StrictMode>
-    <Helmet>
-      {/* <!-- Primary Meta Tags --> */}
-      <title>Yatharth's Portfolio</title>
-      <meta name="title" content="Yatharth's Portfolio" />
-      <meta name="description" content="Web site created using create-react-app" />
-
-      {/* <!-- Open Graph / Facebook --> */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://yatharth-portfolio.vercel.app/" />
-      <meta property="og:title" content="Yatharth's Portfolio" />
-      <meta property="og:description" content="Web site created using create-react-app" />
-      <meta property="og:image" content="/preview1.png" />
-
-      {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://yatharth-portfolio.vercel.app/" />
-      <meta property="twitter:title" content="Yatharth's Portfolio" />
-      <meta property="twitter:description" content="Web site created using create-react-app" />
-      <meta property="twitter:image" content="/preview1.png" />
-    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
