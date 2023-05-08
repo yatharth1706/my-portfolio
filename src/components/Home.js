ef14ba57e82c8c238b06b9287fc3e98e506c540f
yatharth1706 <yatharthverma070@gmail.com>
2023-05-08 20:35:02 +0530

    Improve hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,18 @@ import Services from "./Services";
 
 function Home() {
   return (
-    <div className="flex flex-col md:flex-row  pb-10 items-center justify-center z-10">
+    <div className="flex flex-col md:flex-row items-center justify-center z-10">
       <img
-        src="/GlassMorph1.png"
+        src="/heroBg.jpg"
         alt="hero-section-bg"
-        className="hidden sm:block sm:absolute"
+        className="flex absolute justify-center backdrop-filter backdrop-blur-xl bg-cover w-4/5 top-0"
         style={{ zIndex: "-1000" }}
+      />
+      <img
+        src="/grid.webp"
+        alt="grid"
+        className="flex absolute justify-center backdrop-filter backdrop-blur-xl bg-cover w-full top-0"
+        style={{ zIndex: "-999" }}
       />
       <Intro />
     </div>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,12 +4,12 @@ import { Link } from "react-scroll";
 function Intro() {
   return (
     <div className="flex flex-col h-screen text-white justify-center items-center">
-      <h1 className="relative font-semibold text-center md:text-5xl sm:text-3xl text-xl">
+      <h1 className="relative font-semibold text-center md:text-5xl sm:text-3xl text-xl -mt-28">
         Hi 👋 I am <span style={{ color: "#5A69DE" }}>Yatharth Verma</span>
       </h1>
-      <span className="mt-4 font-normal text-center text-sm">
-        Creative Full Stack Web Developer with 2+ years of experience who love open source and love
-        building cool stuff
+      <span className="mt-4 font-normal text-center text-sm sm:w-4/5">
+        Creative Full Stack Web Developer with 2+ years of experience who love
+        open source and love building cool stuff
       </span>
       <div className="flex space-x-6 mt-3">
         <a
@@ -20,7 +20,8 @@ function Intro() {
           <button
             className="text-gray-200 mt-4 flex  mx-auto md:mx-0 transition-transform duration-300 transform hover:scale-105 text-sm w-32 sm:w-40"
             style={{
-              background: "linear-gradient(89deg, #9a5cff 0%, hsl(228, 64%, 58%) 100%)",
+              background:
+                "linear-gradient(89deg, #9a5cff 0%, hsl(228, 64%, 58%) 100%)",
               borderRadius: "40px",
               padding: "8px",
               justifyContent: "center",
@@ -31,7 +32,7 @@ function Intro() {
         </a>
         <Link to="contactMe" smooth={true}>
           <button
-            className="bg-gray-300 text-gray-900 mt-4 flex  mx-auto md:mx-0 transition-transform duration-300 transform hover:scale-105 w-32 sm:w-40"
+            className="bg-gray-200 text-gray-900 mt-4 flex  mx-auto md:mx-0 transition-transform duration-300 transform hover:scale-105 w-32 sm:w-40"
             style={{
               borderRadius: "40px",
               padding: "8px",
@@ -42,21 +43,40 @@ function Intro() {
           </button>
         </Link>
       </div>
-
-      <div className="flex space-x-8 mt-10 mb-8 mx-auto md:mx-0">
-        <a href="http://github.com/yatharth1706" target="_blank" rel="noreferrer">
-          <i className="fab fa-github  text-gray-300" style={{ fontSize: "24px" }}></i>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/yatharth-verma-938924169/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <i className="fab fa-linkedin-in text-gray-300" style={{ fontSize: "24px" }}></i>
-        </a>
-        <a href="https://twitter.com/yatharth170699" target="_blank" rel="noreferrer">
-          <i className="fab fa-twitter text-gray-300" style={{ fontSize: "24px" }}></i>
-        </a>
+      <div className="flex flex-col space-y-3 mt-16 absolute bottom-10">
+        <p className="text-gray-400">Connect with me in any of the below</p>
+        <div className="flex space-x-8 mb-8 mx-auto md:mx-0 justify-center">
+          <a
+            href="http://github.com/yatharth1706"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i
+              className="fab fa-github  text-gray-300 transform transition-all hover:scale-110"
+              style={{ fontSize: "24px" }}
+            ></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/yatharth-verma-938924169/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i
+              className="fab fa-linkedin-in text-gray-300 transform transition-all hover:scale-110"
+              style={{ fontSize: "24px" }}
+            ></i>
+          </a>
+          <a
+            href="https://twitter.com/yatharth170699"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i
+              className="fab fa-twitter text-gray-300 transform transition-all hover:scale-110"
+              style={{ fontSize: "24px" }}
+            ></i>
+          </a>
+        </div>
       </div>
     </div>
   );
